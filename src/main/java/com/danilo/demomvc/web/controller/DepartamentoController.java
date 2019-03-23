88137d0a1e1b52d12f5e3ca442e1a1c183e39361
Danilo Valério da Silva <danilo_vs@hotmail.com>
2019-03-22 23:22:01 -0300

    Cadastro de departamento funcionando

--- src/main/java/com/danilo/demomvc/web/controller/DepartamentoController.java
+++ src/main/java/com/danilo/demomvc/web/controller/DepartamentoController.java
@@ -1,15 +1,23 @@
 package com.danilo.demomvc.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.danilo.demomvc.domain.Departamento;
+import com.danilo.demomvc.service.DepartamentoService;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
 	
+	@Autowired
+	private DepartamentoService service; //para termos acesso aos métodos do service
+	
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Departamento departamento) {
 		return "/departamento/cadastro";
 	}
 	
@@ -17,6 +25,15 @@ public class DepartamentoController {
 	public String listar() {
 		return "/departamento/lista";
 	}
+	
+	//Método para cadastrar os dados que vem do formulário através do submit
+	@PostMapping("/salvar")
+	public String salvar(Departamento departamento) {
+		service.salvar(departamento);
+		return "redirect:/departamentos/cadastrar"; //após salvar redireciona para o cadastrar dep.
+		
+	}
+	
 	
 	
 
