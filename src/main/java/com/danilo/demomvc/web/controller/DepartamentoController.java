4189e5ba460a674ff0c99811ff467d95c0456010
danilovalerio <danilo_vs@hotmail.com>
2019-03-25 19:24:29 -0300

    Editando Departamentos

--- src/main/java/com/danilo/demomvc/web/controller/DepartamentoController.java
+++ src/main/java/com/danilo/demomvc/web/controller/DepartamentoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -36,6 +37,23 @@ public class DepartamentoController {
 		return "redirect:/departamentos/cadastrar"; //após salvar redireciona para o cadastrar dep.
 		
 	}
+	
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("departamento", service.buscarPorId(id));
+		return "/departamento/cadastro";
+		
+	}
+	
+	@PostMapping("/editar")
+	public String editar(Departamento departamento) {
+		service.editar(departamento);
+		return "redirect:/departamentos/cadastrar";
+	}
+	
+	
+	
+	
 	
 	
 	
