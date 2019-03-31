a93e870a829aa7d4e040dbc8db0fbb4f2e6582ae
Danilo Valério da Silva <danilo_vs@hotmail.com>
2019-03-31 20:16:50 -0300

    Inserir cargos, com erro no tipo de departamento na listagem do combobox

--- src/main/java/com/danilo/demomvc/web/controller/CargoController.java
+++ src/main/java/com/danilo/demomvc/web/controller/CargoController.java
@@ -1,15 +1,31 @@
 package com.danilo.demomvc.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.danilo.demomvc.domain.Cargo;
+import com.danilo.demomvc.domain.Departamento;
+import com.danilo.demomvc.service.CargoService;
+import com.danilo.demomvc.service.DepartamentoService;
 
 @Controller
 @RequestMapping("/cargos")
 public class CargoController {
 	
+	@Autowired
+	private CargoService cargoService; //para acessar métodos do service
+	@Autowired
+	private DepartamentoService departamentoService;
+	
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Cargo cargo) {
 		return "/cargo/cadastro";
 	}
 	
@@ -17,5 +33,20 @@ public class CargoController {
 	public String listar() {
 		return "/cargo/lista";
 	}
+	
+	//Realiza o cadastro do cargo
+	@PostMapping("/salvar")
+	public String salvar(Cargo cargo, RedirectAttributes attr) {
+		cargoService.salvar(cargo);
+		attr.addFlashAttribute("success", "Cargo inserido com sucesso.");
+		return "redirect:/cargos/cadastrar";//após salvamento redireciona para o acadastro
+	}
+	
+	//Exibe a lista de departamentos no combobox para selecionar ao cadastrar cargo
+	@ModelAttribute("departamentos")
+	public List<Departamento> listaDeDepartamentos(){
+		return departamentoService.buscarTodos();
+	}
+	
 
 }
