26b08cc3719d275eb2eaa5964f01af80d0f64043
danilovalerio <danilo_vs@hotmail.com>
2019-04-15 13:04:01 -0300

    Conversão de String para BigDecimal ok

--- src/main/java/com/danilo/demomvc/web/conversor/StringToBigDecimal.java
+++ src/main/java/com/danilo/demomvc/web/conversor/StringToBigDecimal.java
@@ -5,13 +5,23 @@ import java.math.BigDecimal;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class StringToBigDecimal implements Converter<String, BigDecimal> {
 
 	@Override
 	public BigDecimal convert(String text) {
-		//text = text.trim(); //para remover espaço em branco ao final da informaçaõ 		
-		BigDecimal bd = new BigDecimal(text = text.trim());
+		System.out.println("Valor que está chegando: "+text);
+		text = text.trim(); //para remover espaço em branco ao final da informação
+		String text2 = text.replace(".","");
+		System.out.println("Valor sem ponto: "+text2);
+		String text3 = text2.replaceAll(",",".");
+		System.out.println("Troca da vírgula por ponto: "+text3);
+		
+				
+		
+		BigDecimal bd = new BigDecimal(text3);
+		System.out.println("Valor que está saindo: "+bd);
+		
 		return bd;
 	}
 
